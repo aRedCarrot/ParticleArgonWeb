57e43655361eef5c045a98e1fddfd3e098c0dd27
Nunners <aRedCarrot@gmail.com>
2021-05-26 21:55:47 -0400

    Everything should work

--- argon/src/WeatherStation.ino
+++ argon/src/WeatherStation.ino
@@ -1,9 +1,9 @@
 // BELJ2434 - FONH3001
-// SYSTEM_MODE(MANUAL);
+SYSTEM_MODE(SEMI_AUTOMATIC);
 #include <math.h>
 #include "../lib/google-maps-device-locator/src/google-maps-device-locator.h"
 #include "../lib/JsonParserGeneratorRK/src/JsonParserGeneratorRK.h"
-// JSON DATA
+// JSON DATA FIELDS
 float _lat = 0; // float
 float _lon = 0; // float
 float _acc = 0; // float
@@ -16,23 +16,27 @@ int _tempInt = 0; // Nombre
 int _tempDec = 0; // Nombre
 int _pression = 0; // Kilo pascal
 
+// Argon pins
 const int LightPin = A0;
 const int PluviometrePin = A5;
 const int AnemometreVitessePin = A3;
 const int AnemometreDirectionPin = A2;
 const int TempHumidityPin_1 = D2;
-//const int TempHumidityPin_2 = D3;
+
+// Google map locator
 GoogleMapsDeviceLocator locator;
 bool locationPosted = false;
+
+// TCP client
 TCPClient client;										// Create TCP Client object
 byte server[] = {192, 168, 0, 103}; // http://maker-io-iot.atwebpages.com/
 byte dataBuffer[1024];
-
 String receivedData;
 
+// Last time the sensor values were sent to the server
 time_t lastTimePosted = 0;
 
-// Barometre values
+// Pressure sensor values
 int kTFactor = 7864320;
 int kPFactor = 7864320;
 int32_t c0,c1,c00,c10,c01,c11,c20,c21,c30 = {0};
@@ -50,6 +54,7 @@ void locationCallback(float lat, float lon, float acc)
 	locationPosted = true;
 }
 
+// Converts a number to it's two's complement value
 void getTwosComplement(int32_t *raw, uint8_t length)
 {
     if (*raw & ((uint32_t)1 << (length - 1)))
@@ -58,6 +63,7 @@ void getTwosComplement(int32_t *raw, uint8_t length)
     }
 }
 
+// Read a byte in I2C from the slave device
 uint8_t readByte(uint8_t slaveDevice ,uint8_t regAddress)
 {
     Wire.beginTransmission(slaveDevice);
@@ -68,6 +74,7 @@ uint8_t readByte(uint8_t slaveDevice ,uint8_t regAddress)
     return Wire.read(); //return this byte on success
 }
 
+// Write a byte in I2C to the slave device
 void writeByte(uint8_t slaveDevice ,uint8_t regAddress, uint8_t value, uint8_t mask = 0xFF)
 {
 		int16_t old = readByte(slaveDevice,regAddress);
@@ -80,17 +87,19 @@ void writeByte(uint8_t slaveDevice ,uint8_t regAddress, uint8_t value, uint8_t m
 
 void setup()
 {
-	Serial.begin(9600);
-	Wire.begin();
-	lastTimePosted = Time.now();
+	Serial.begin(9600); // Starts serial monitor and baud rate
+	Wire.begin(); // Starts the I2C bus
+	lastTimePosted = Time.now(); // Sets the first time
+	Particle.connect(); // Connect to particle cloud services
 	// Google map locator API
 	locator.withSubscribe(locationCallback).withLocateOnce();
+	// Sets pin modes
 	pinMode(LightPin,INPUT);
 	pinMode(AnemometreVitessePin,INPUT);
 	pinMode(AnemometreDirectionPin,INPUT);
 	pinMode(PluviometrePin,INPUT);
 
-	// Wait for values to be ready
+	// Wait for pressure coefs to be ready
 	bool coefReady = false;
 	bool sensorReady = false;
 	while(!coefReady || !sensorReady){
@@ -103,7 +112,7 @@ void setup()
 		}
 		delay(100);
 	}
-	// Compensated value
+	// Pressure sensor coefficients
 	c0 = (readByte(slave,0x10) << 4) | ((readByte(slave,0x11) & 0xF0) >> 4);
 	c1 = ((readByte(slave,0x11) & 0x0F) << 8 ) | (readByte(slave,0x12));
 	getTwosComplement(&c0,12);
@@ -130,7 +139,7 @@ void setup()
 	c30 = (readByte(slave,0x20) << 8) | readByte(slave,0x21);
 	getTwosComplement(&c30,16);
 
-	// Setup all registers for I2C pressure
+	// Setup registers for I2C pressure sensor
 	writeByte(slave,0x07, 0x20, 0x70);  // Set pressure config
 	writeByte(slave,0x07, 0x03, 0x07);
 	writeByte(slave,0x06, 0x20, 0x70);  // Set temp config
@@ -149,25 +158,34 @@ void setup()
 
 void loop()
 {
-	testLightSensor();
-	delay(250);
-	testAnemometre();
-	delay(250);
-	testPluviometre();
-	delay(250);
+	// Test all sensors one by one
 	TestBarometre();
-	delay(250);
+	delay(150);
+	testLightSensor();
+	delay(150);
+	testAnemometre();
+	delay(150);
+	testPluviometre();
+	delay(150);
+	TestBarometre();
+	delay(150);
 	testTemperatureAndHumidity();
 	time_t now = Time.now();
 	if(!locationPosted){
 		// Google map locator API
 		locator.loop();
 	} else {
-		if(now - lastTimePosted > 5){
-			WiFi.connect();
-			while(!WiFi.ready()){delay(100);}
-			connectAndSendAllToServer();
-			WiFi.disconnect();
+		if(now - lastTimePosted > 5){ // Post a new result every X seconds
+			Particle.connect();
+			WiFi.on();
+			Serial.println("Connecting");
+			if(WiFi.ready()){
+				Serial.println("Connected");
+				connectAndSendAllToServer();
+				Particle.disconnect(); // Disconnect and turn off wifi to save power
+				WiFi.off();
+				Serial.println("Disconnected");
+			}
 		}
 	}
 }
@@ -178,6 +196,7 @@ void testAnemometre()
 	bool previouslyOn = false;
 	int speed = 0;
 	int impulse = 0;
+	// Check the number of impulse in 1 seconds
 	for(int i = 0; i < 1000; i++)
 	{
 		speed = analogRead(AnemometreVitessePin);
@@ -190,7 +209,7 @@ void testAnemometre()
 		}
 		delay(1);
 	}
-	speed = 2.4 * impulse;
+	speed = 2.4 * impulse; // We know 1 impulse per second is equal to 2.4 km/h
 	int direction = analogRead(AnemometreDirectionPin);
 	float voltageChart[16] = {3.84,1.98,2.25,0.41,0.45,0.32,0.90,0.62,1.40,1.19,3.08,2.93,4.62,4.04,4.33,3.43};
 	const float angleChart[16] = {0,22.5,45,67.5,90,112.5,135,157.5,180,202.5,225,247.5,270,292.5,315,337.5};
@@ -200,7 +219,7 @@ void testAnemometre()
 	float angle = -1;
 	for(int i = 0; i < 16;i++){
 		if((direction > voltageChart[i] - 50) && (direction < voltageChart[i] + 50)){
-			angle = angleChart[i];
+			angle = angleChart[i]; // Find the angle that best fits the voltage we get
 		}
 	}
 	_speed = speed;
@@ -212,7 +231,7 @@ void testPluviometre()
 	bool previouslyOn = true;
 	int impulse = 0;
 	float pluie;
-	for(int i = 0; i < 10; i++)
+	for(int i = 0; i < 30; i++) // Since this sensor is very bad, give it 3 seconds to calculate the amount of rain
 	{
 		pluie = analogRead(PluviometrePin);
 		if(pluie > 1000 && !previouslyOn){
@@ -224,20 +243,24 @@ void testPluviometre()
 		}
 		delay(100);
 	}
-	pluie = 0.2794 * impulse;
+	pluie = ((0.2794 * impulse) / 3) * 60; // Calculate rain per min
 	_pluie = pluie;
 }
 
 void testLightSensor()
 {
-	int result = analogRead(LightPin);
-	_lumiere = result;
+	float analogIn = analogRead(LightPin);
+	//Serial.printlnf(" Analog in : %f", analogIn);
+	float saturation = 2550; // Saturation
+	float lux = analogIn/saturation * 100; // Mise a l echelle sur 100 lux maximum
+	_lumiere = lux;
+	Serial.printlnf("Lux : %f",lux);
 }
 
 void testTemperatureAndHumidity()
 {
 	uint8_t data[40] = {0};
-	noInterrupts();
+	noInterrupts(); // Pause interupts since this code is extremely time sensitive
 	pinMode(TempHumidityPin_1, OUTPUT);
 	// pull the pin high and wait 250 milliseconds
 	digitalWrite(TempHumidityPin_1, HIGH);
@@ -249,11 +272,13 @@ void testTemperatureAndHumidity()
 	delayMicroseconds(40);
 	pinMode(TempHumidityPin_1, INPUT_PULLUP);
 	pulseIn(TempHumidityPin_1,HIGH);
+	// Record the 40 bit message
 	for(int i = 0; i < 40; i++){
 		data[i] = pulseIn(TempHumidityPin_1,HIGH);
 	}
 	interrupts();
 	byte bytes[5] = {0};
+	// Organise the data
 	for(int i = 0; i < 5; i++)
 	{
 		for(int j = 0; j < 8; j++){
@@ -263,11 +288,13 @@ void testTemperatureAndHumidity()
 			}
 		}
 	}
+	// Validate the checksum
 	if (bytes[4] != ((bytes[0] + bytes[1] + bytes[2] + bytes[3]) & 0xFF)) {
 			Serial.println("Checksum invalid");
 			return;
 	}
 	Serial.println("Checksum valid");
+	// Fetch values
 	int humidityInteger = bytes[0];
 	int humidityDecimal = bytes[1];
 	int tempInteger = bytes[2];
@@ -282,16 +309,17 @@ void testTemperatureAndHumidity()
 
 void connectAndSendAllToServer()
 {
+
 	JsonWriterStatic<512> jw;
 	{
 		JsonWriterAutoObject obj(&jw);
 		jw.insertKeyValue("Vitesse anemometre(Km/h)", _speed);
 		jw.insertKeyValue("Angle anemometre(Degree)", _angle);
-		jw.insertKeyValue("Pluie(mm/s)",_pluie);
+		jw.insertKeyValue("Pluie(mm/m)",_pluie);
 		jw.insertKeyValue("Lumiere(Lux)",_lumiere);
 		jw.insertKeyValue("Humidite(%)",_humid);
-		jw.insertKeyValue("TempInteger(Capteur humid)",_tempInt);
-		jw.insertKeyValue("TempDecimal(Capteur humid)",_tempDec);
+		jw.insertKeyValue("Temperature(Capteur humid, Celcius)",String(_tempInt) + "." + String(_tempDec));
+		//jw.insertKeyValue("TempDecimal(Capteur humid)",_tempDec);
 		jw.insertKeyValue("Pression (KPascal)",_pression);
 	}
 	String jsonObj(jw.getBuffer());
@@ -325,7 +353,7 @@ void TestBarometre()
 		delay(100);
 	}
 
-	// // Temperature
+	// Read temperature
 	reg_temp[0] = readByte(slave,0x03);
 	reg_temp[1] = readByte(slave,0x04);
 	reg_temp[2] = readByte(slave,0x05);
@@ -340,8 +368,6 @@ void TestBarometre()
   rawTemp = temp;
 	// Scale it
   temp = ( c0 / 2.0 ) + c1 * temp;
-	Serial.print("Temperature celcius : ");
-	Serial.println(String(temp));
 
 	writeByte(slave,0x08,0x01); // Enable pressure measurement
 	delay(200);
@@ -363,6 +389,7 @@ void TestBarometre()
 	float pression = rawPressure;
 	pression /= kPFactor;
 	
+	// Calculate compensated pressure value using temp and coefs
 	pression = c00 + pression * (c10 + pression * (c20 + pression * c30)) + rawTemp * (c01 + pression * (c11 + pression * c21));
 	Serial.print("Barometre (KiloPascal) : ");
 	Serial.println(String(pression/1000));
